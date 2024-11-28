06239196183e4f5ccce9c391fe429b090f552161
3254c <chikara8460@icloud.com>
2024-11-28 15:36:41 +0900

    wrote README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# scSurv
-a deep generative model for single-cell survival analysis
+# scSurv (a deep generative model for single-cell survival analysis)
+scSurv is a novel method for single-cell survival analysis.
+
+[Teppei Shimamura's lab](https://www.shimamlab.info/), Tokyo Medical and Dental university at Tokyo and Nagoya University at Nagoya
+
+[Yasuhiro Kojima's lab](https://www.ncc.go.jp/jp/ri/division/computational_life_science),   National Cancer Center Research Institute at Tokyo
+
+Developed by Chikara Mizukoshi
+
+# Instalation
+You can use the latest development version from GitHub.
+```
+!git clone https://github.com/3254c/scSurv.git
+```
+# Usage
+You need to prepare reference scRNA-seq data and bulk RNA-seq data linked to clinical outcome. For example, TCGA bulk RNA-seq data is available from the Genomic Data Commons (GDC) Data Portal (https://portal.gdc.cancer.gov/). For examples of reference scRNA-seq data, please refer to our paper. 
+
+In the tutorial, we demonstrate the usage of scSurv using simulated data.
+The bulk_adata.obs must contain the survival outcome data and the event indicators (censoring status).
+The scRNA-seq and bulk RNA-seq data must be raw counts.
+Please specify the batch_key parameter to indicate the sample origin annotation for each cell.
+```
+batch_key = 'orig.ident'
+exp_name = 'tutolial_simulation.pt'
+epoch = 10000
+
+sc_adata, bulk_adata, model_params_dict, spatial_adata, vaesm_exp = workflow.run_scSurv(sc_adata, bulk_adata, exp_name, epoch, batch_key)
+```
+![visualization celltype annotation](umap_celltype_minor.png)
+![visualization setting and estimated contribution](umap_setting_and_estimated_beta.png)
--- tutolial_simulation.py
+++ tutolial_simulation.py
@@ -1,9 +1,11 @@
+#The scRNA-seq data used in this simulation is derived from the study by Wu et al. (https://doi.org/10.1038/s41588-021-00911-1).
 import src.scsurv.workflow as workflow
 import pandas as pd
 import scanpy as sc
+import matplotlib.pyplot as plt
 
-sc_adata = sc.read_h5ad('/home/mizukoshi/scSurv/simulated_BRCA_5000genes_CID4471_celltype_minor_300_major_reso0.1_sc_reference.h5ad')
-bulk_adata = sc.read_h5ad(f'/home/mizukoshi/scSurv/single_beta_bulk_BRCA_betascale5_alive0_major_reso0.1_0.h5ad')
+sc_adata = sc.read_h5ad('/home/mizukoshi/scSurv/tutolial_simulated_sc_reference.h5ad')
+bulk_adata = sc.read_h5ad('/home/mizukoshi/scSurv/tutolial_simulated_bulk.h5ad')
 bulk_ncells_per_leiden = bulk_adata.uns['n_cells_per_celltype_df'].T
 setting_beta = pd.Series(bulk_adata.uns['setting_beta'], index = bulk_ncells_per_leiden.columns)
 cell_type_df = sc_adata.obs['celltype_minor']
@@ -14,9 +16,16 @@ exp_name = f'tutolial_simulation.pt'
 epoch = 10000
 
 sc_adata, bulk_adata, model_params_dict, spatial_adata, vaesm_exp = workflow.run_scSurv(sc_adata, bulk_adata, exp_name, epoch, batch_key)
-sc.pl.umap(sc_adata, color = ['celltype_minor'], save = '_celltype_minor.png')
-sc.pl.umap(sc_adata, color = ['setting_beta'], save = '_setting_beta.png')
-sc.pl.umap(sc_adata, color = ['beta_z'], save = '_estimated_beta.png')
+
+fig, ax = plt.subplots(figsize=(14, 6))
+sc.pl.umap(sc_adata, color='celltype_minor', ax=ax, show=False, colorbar_loc = None, frameon = False, title='celltype_minor')
+fig.subplots_adjust(right=0.5)
+plt.savefig('umap_celltype_minor.png')
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+sc.pl.umap(sc_adata, color='setting_beta', ax=ax1, show=False, colorbar_loc=None, frameon=False, title='Setting contribution')
+sc.pl.umap(sc_adata, color='beta_z', ax=ax2, show=False, colorbar_loc=None, frameon=False, title='Estimated contribution')
+plt.savefig('umap_setting_and_estimated_beta.png')
 
 estimated_p_sc_level = pd.DataFrame(sc_adata.obsm['map2bulk'].T, index=bulk_adata.obs_names, columns=sc_adata.obs_names)
 bulk_ncells = bulk_adata.uns['n_cells_per_celltype_df'].T
@@ -31,4 +40,4 @@ print('scSurv deconvolution correlation', deconv_corr.median())
 beta_means = {item: sc_adata[sc_adata.obs['celltype_minor'] == item].obs['beta_z'].mean() 
              for item in bulk_ncells.columns}
 beta_corr = pd.Series(beta_means).corr(setting_beta)
-print('scSurv beta correlation', beta_corr)
+print('scSurv contribution correlation', beta_corr)
